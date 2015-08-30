397be71fdeed85501478abdf93f1fa5d8bb75051
andrey <Kurienkoand@gmail.com>
2015-08-30 19:06:18 +0300

    css : add working gallery

--- script/gallery.js
+++ script/gallery.js
@@ -1,36 +1,175 @@
-$(document).ready(function () {
-    $('.thumbs').each(function () {
-        var PhotoSetID = $(this).attr("id");
+$(document).ready(function() {
+    /*
+     *  Simple image gallery. Uses default settings
+     */
 
-        var url = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=f0b84fba1c00631410b85b90720f52ba";
-        var src;
-        $.getJSON(url + "&photoset_id=" + PhotoSetID + "&format=json&jsoncallback=?", function (data) {
-            $.each(data.photoset.photo, function (i, item) {
-                src = "http://farm" + item.farm + ".static.flickr.com/" + item.server + "/" + item.id + "_" + item.secret + ".jpg";
-                title = item.title;
+    $('.fancybox').fancybox();
 
+    /*
+     *  Different effects
+     */
 
-                $("<a>").attr({
-                    "href": src,
-                    "class": "gallery",
-                    "rel" : PhotoSetID,
-                    "title": title
-                }).html(
-                    $("<img />").attr({
-                        "src": src,
-                        "alt": title,
-                        "class": (i === 0 ? "show" : "hide"),
-                        "width": "240",
-                        "height": "180"
-                    })
-
-                ).appendTo("#" + PhotoSetID);
-                $("a.gallery[rel=" + PhotoSetID +"]").colorbox();
-
-            });
-
-        });
-
+    // Change title type, overlay closing speed
+    $(".fancybox-effects-a").fancybox({
+        helpers: {
+            title : {
+                type : 'outside'
+            },
+            overlay : {
+                speedOut : 0
+            }
+        }
     });
 
+    // Disable opening and closing animations, change title type
+    $(".fancybox-effects-b").fancybox({
+        openEffect  : 'none',
+        closeEffect	: 'none',
+
+        helpers : {
+            title : {
+                type : 'over'
+            }
+        }
+    });
+
+    // Set custom style, close if clicked, change title type and overlay color
+    $(".fancybox-effects-c").fancybox({
+        wrapCSS    : 'fancybox-custom',
+        closeClick : true,
+
+        openEffect : 'none',
+
+        helpers : {
+            title : {
+                type : 'inside'
+            },
+            overlay : {
+                css : {
+                    'background' : 'rgba(238,238,238,0.85)'
+                }
+            }
+        }
+    });
+
+    // Remove padding, set opening and closing animations, close if clicked and disable overlay
+    $(".fancybox-effects-d").fancybox({
+        padding: 0,
+
+        openEffect : 'elastic',
+        openSpeed  : 150,
+
+        closeEffect : 'elastic',
+        closeSpeed  : 150,
+
+        closeClick : true,
+
+        helpers : {
+            overlay : null
+        }
+    });
+
+    /*
+     *  Button helper. Disable animations, hide close button, change title type and content
+     */
+
+    $('.fancybox-buttons').fancybox({
+        openEffect  : 'none',
+        closeEffect : 'none',
+
+        prevEffect : 'none',
+        nextEffect : 'none',
+
+        closeBtn  : false,
+
+        helpers : {
+            title : {
+                type : 'inside'
+            },
+            buttons	: {}
+        },
+
+        afterLoad : function() {
+            this.title = 'Image ' + (this.index + 1) + ' of ' + this.group.length + (this.title ? ' - ' + this.title : '');
+        }
+    });
+
+
+    /*
+     *  Thumbnail helper. Disable animations, hide close button, arrows and slide to next gallery item if clicked
+     */
+
+    $('.fancybox-thumbs').fancybox({
+        prevEffect : 'none',
+        nextEffect : 'none',
+
+        closeBtn  : false,
+        arrows    : false,
+        nextClick : true,
+
+        helpers : {
+            thumbs : {
+                width  : 50,
+                height : 50
+            }
+        }
+    });
+
+    /*
+     *  Media helper. Group items, disable animations, hide arrows, enable media and button helpers.
+     */
+    $('.fancybox-media')
+        .attr('rel', 'media-gallery')
+        .fancybox({
+            openEffect : 'none',
+            closeEffect : 'none',
+            prevEffect : 'none',
+            nextEffect : 'none',
+
+            arrows : false,
+            helpers : {
+                media : {},
+                buttons : {}
+            }
+        });
+
+    /*
+     *  Open manually
+     */
+
+    $("#fancybox-manual-a").click(function() {
+        $.fancybox.open('1_b.jpg');
+    });
+
+    $("#fancybox-manual-b").click(function() {
+        $.fancybox.open({
+            href : 'iframe.html',
+            type : 'iframe',
+            padding : 5
+        });
+    });
+
+    $("#fancybox-manual-c").click(function() {
+        $.fancybox.open([
+            {
+                href : '1_b.jpg',
+                title : 'My title'
+            }, {
+                href : '2_b.jpg',
+                title : '2nd title'
+            }, {
+                href : '3_b.jpg'
+            }
+        ], {
+            helpers : {
+                thumbs : {
+                    width: 75,
+                    height: 50
+                }
+            }
+        });
+    });
+
+
 });
+
